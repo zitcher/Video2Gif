185538a0a5ef385d0a6298cda299b3d2a8e96864
Zachary Hoffman <hoffking@gmail.com>
2020-12-03 01:02:22 -0500

    pooling head

--- embedding_test.py
+++ embedding_test.py
@@ -9,13 +9,13 @@ from array2gif import write_gif
 
 if __name__ == "__main__":
     urls = [
-        'https://38.media.tumblr.com/8fd8930ec4e3458c1e616d6002e6a53f/tumblr_nqq3ibcw841rvg72ao1_400.gif',
-        'https://33.media.tumblr.com/898ee85e0a05685d9c0cb326f9fd2280/tumblr_noijlk8qnH1uvid27o1_500.gif',
-        'https://38.media.tumblr.com/7c3b37e8b79918eddb6fefb01d814bfe/tumblr_nmwkmcsEvf1tyncywo1_500.gif'
+        'https://33.media.tumblr.com/ab7342b8cf218a50fc80b59896393b9e/tumblr_nq3ym5BfvM1uv0y2ro1_250.gif',
+        'https://33.media.tumblr.com/b9e43f1f2905aa9d2b92a58591372b83/tumblr_nnuv5ro5eX1td4njbo1_400.gif',
+        'https://33.media.tumblr.com/4ccfa9269be73b6ecf451e035707558d/tumblr_no7b56gmOQ1qa05h5o1_400.gif'
     ]
 
     tmp_path = './tmp.gif'
-    net = ResNet101(weights='imagenet', include_top=False)
+    net = ResNet101(weights='imagenet', include_top=False, pooling='avg')
 
     embeddings = []
     for i in range(len(urls)):
@@ -23,15 +23,12 @@ if __name__ == "__main__":
             f.write(requests.get(urls[i]).content)
     
             frames = [np.array(frame.copy().convert('RGB').getdata(), dtype=np.uint8).reshape(frame.size[1], frame.size[0], 3) for frame in ImageSequence.Iterator(Image.open(tmp_path))]
-            step = math.ceil(len(frames) / 5)
+            step = math.ceil(len(frames) / 3)
             sampled_frames = np.array([frames[i] for i in range(step // 2, len(frames), step)])
             inp = tf.image.resize(preprocess_input(sampled_frames), [244, 244], antialias=True)
+            print(inp.shape)
             inp = net.predict(inp)
-            inp = np.mean(inp, axis=0)
-            embeddings.append(inp.flatten())
+            print(inp.shape)
+            embeddings.append(np.mean(inp, axis=0))
 
     print(np.linalg.norm(embeddings[0] - embeddings[1]), np.linalg.norm(embeddings[0] - embeddings[2]))
-
-            
-        
-        
